a03533b22e457a16a284053df708a740465b1644
Cauã de Lima Araújo <20231144010003@ifrn.local>
2024-12-02 16:20:21 -0300

    comandos no mysql

---
+++ migrations/script1.sql
@@ -0,0 +1,26 @@
+create table projeto (
+   	id int AUTO_INCREMENT PRIMARY KEY,
+    nome varchar(300) not null
+);
+
+
+
+-- Exemplos de adição de registros
+INSERT INTO projeto (nome) VALUES ('XXX');
+
+-- Exemplos de consulta
+SELECT * FROM projeto; -- Todas as colunas
+SELECT id FROM projeto; -- Só a coluna id
+SELECT nome FROM projeto; -- Só a coluna nome
+SELECT id, nome FROM projeto; -- As duas colunas
+
+-- Exemplos de consultas com where (condição)
+SELECT * FROM projeto WHERE nome LIKE 'Gu%';
+SELECT * FROM projeto WHERE id = 2;
+
+-- Exemplo de exclusão de registros (PERIGOSO!)
+-- delete from projeto;
+-- delete from projeto where id = 3; (MENOS PERIGOSO MAS AINDA É PERIGOSO KKK)
+
+-- Atualizar registros
+UPDATE projeto SET nome = 'XXX' WHERE id = 1;
